e075ce7d604cc7786ff8ff7ec21676ef772198f8
MrSayyod <ssayyod@gmail.com>
2020-06-22 19:35:58 +0500

    Adds update method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,7 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
-
+  
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
@@ -54,6 +54,11 @@ class ArticlesController < ApplicationController
   def edit
     @article = Article.find(params[:id])
   end
-
   
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
 end
